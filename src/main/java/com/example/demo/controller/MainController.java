0ddc4781da7645efce8554303c61657a057765cc
toolstik <tolstik.va@gmail.com>
2018-08-25 05:10:04 +0700

    user list on view

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,13 +1,21 @@
 package com.example.demo.controller;
  
+import java.util.List;
+import com.example.demo.dal.dao.User;
+import com.example.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
  
 @Controller
 public class MainController {
  
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(value = { "/", "/home" }, method = RequestMethod.GET)
     public String homePage(Model model) {
         return "homePage";
@@ -28,8 +36,12 @@ public class MainController {
     }
 
     @RequestMapping(value = { "/users" }, method = RequestMethod.GET)
-    public String usersPage(Model model) {
-        return "usersPage";
+    public ModelAndView usersPage(Model model) {
+        ModelAndView result = new ModelAndView();
+        result.setViewName("usersPage");
+        List<User> users = userService.findAll();
+        result.addObject("users", users);
+        return result;
     }
      
 }
--- src/main/webapp/WEB-INF/bodyfragments/_users.jsp
+++ src/main/webapp/WEB-INF/bodyfragments/_users.jsp
@@ -1,3 +1,31 @@
-<h2>Users</h2>
- 
-This is user list page
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Roles</th>
+      <th>Creation date</th>
+      <th>Gender</th>
+    </tr>
+  </thead>
+  <tbody>
+    <c:if test="${empty users}">
+      <tr>
+        <td colspan="8">No users to display</td>
+      </tr>
+    </c:if>
+    <c:if test="${not empty users}">
+
+      <c:forEach items="${users}" var="user">
+        <tr class="">
+          <td>${user.name}</td>
+          <td>${user.roles}</td>
+          <td>${user.createDate}</td>
+          <td>${user.gender}</td>
+        </tr>
+      </c:forEach>
+    </c:if>
+  </tbody>
+</table>
+</div>
